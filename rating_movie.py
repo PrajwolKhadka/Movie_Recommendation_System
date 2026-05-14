133d56df048a140aa2d6728dfe104ebb5cb7b3b9
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-05-14 19:32:02 +0545

    reading into rating file

--- rating_movie.py
+++ rating_movie.py
@@ -1,6 +1,8 @@
 import pandas as pd
-#Loading the ratings dataset
+# #Loading the ratings dataset
 def load_ratings():
     ratings_csv = pd.read_csv('ml-25m/ratings.csv')
     return ratings_csv
-print(load_ratings().head())
+print(load_ratings().head(n=10))
+# ratings_csv = pd.read_csv('ml-25m/ratings.csv')
+# print(ratings_csv.dtypes)
