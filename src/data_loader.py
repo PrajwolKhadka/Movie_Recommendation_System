878d0c6e079d1a507367f98d19bfea71d1a55732
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-06-14 15:27:41 +0545

    fix(data_loader and rating_movie):import fixed

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,19 @@
+# import pandas as pd
+# from preprocessing import clean_title
+# #Loading the movies dataset
+# def load_movies():
+#     movie_csv = pd.read_csv('ml-25m/movies.csv')
+#     movie_csv["clean_title"] = movie_csv["title"].apply(clean_title)
+#     return movie_csv
+
 import pandas as pd
+import os
 from preprocessing import clean_title
-#Loading the movies dataset
+
+# Loading the movies dataset
 def load_movies():
-    movie_csv = pd.read_csv('ml-25m/movies.csv')
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    movie_csv = pd.read_csv(os.path.join(base_dir, '..', 'ml-25m', 'movies.csv'))
     movie_csv["clean_title"] = movie_csv["title"].apply(clean_title)
     return movie_csv
-
-
 
--- src/rating_movie.py
+++ src/rating_movie.py
@@ -1,8 +1,16 @@
+# import pandas as pd
+# # #Loading the ratings dataset
+# def load_ratings():
+#     ratings_csv = pd.read_csv('ml-25m/ratings.csv')
+#     return ratings_csv
+# # print(load_ratings().head(n=10))
+# # ratings_csv = pd.read_csv('ml-25m/ratings.csv')
+# # print(ratings_csv.dtypes)
+
 import pandas as pd
-# #Loading the ratings dataset
+import os
+
 def load_ratings():
-    ratings_csv = pd.read_csv('ml-25m/ratings.csv')
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    ratings_csv = pd.read_csv(os.path.join(base_dir, '..', 'ml-25m', 'ratings.csv'))
     return ratings_csv
-# print(load_ratings().head(n=10))
-# ratings_csv = pd.read_csv('ml-25m/ratings.csv')
-# print(ratings_csv.dtypes)
